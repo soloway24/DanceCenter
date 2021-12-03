c97007d896695cf4786dfb13e269a867785cf0c3
Illia Kuznets <illiakuznets@gmail.com>
2021-12-03 23:55:48 +0200

    first commit

---
+++ src/main/java/com/kuznets/danceCenter/DanceCenterApplication.java
@@ -0,0 +1,13 @@
+package com.kuznets.danceCenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanceCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanceCenterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kuznets/danceCenter/DanceCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuznets.danceCenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanceCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
